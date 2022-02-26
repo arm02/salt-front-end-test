18e48a7b9e750834daa1ca38a6a0b155fcb820f3
arm02 <arm.adrian02@gmail.com>
2022-02-26 22:10:56 +0700

    11 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllVideosComponent } from './content/videos/all-videos/all-videos.component';
+import { AllDocumentComponent } from './content/document/all-document/all-document.component';
+import { AllPeopleComponent } from './content/people/all-people/all-people.component';
+import { AllActivityComponent } from './content/activity/all-activity/all-activity.component';
+import { AllChannelComponent } from './content/channel/all-channel/all-channel.component';
 import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
@@ -14,6 +18,22 @@ const appRoutes: Routes = [
       {
         path: 'videos',
         component: AllVideosComponent,
+      },
+      {
+        path: 'document',
+        component: AllDocumentComponent,
+      },
+      {
+        path: 'people',
+        component: AllPeopleComponent,
+      },
+      {
+        path: 'activity',
+        component: AllActivityComponent,
+      },
+      {
+        path: 'channel',
+        component: AllChannelComponent,
       },
     ]
   }
