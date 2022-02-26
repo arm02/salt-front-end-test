17983dbcc963b16432adedf77da8d5b770de31e7
arm02 <arm.adrian02@gmail.com>
2022-02-26 19:11:56 +0700

    1 file modified

--- src/app/partials/header/header.component.ts
+++ src/app/partials/header/header.component.ts
@@ -1,8 +1,20 @@
 import { Component, VERSION } from '@angular/core';
+import { SignInComponent } from 'src/app/auth/signin/signin.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogRef = this.dialog.open(SignInComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+}
