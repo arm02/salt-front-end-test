99676684c7429ec9b431a8ccf5ff1c0069524c47
arm02 <arm.adrian02@gmail.com>
2022-02-26 23:41:32 +0700

    3 files modified

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,6 +1,8 @@
-import { Component, VERSION } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { Component, VERSION, ViewChild } from '@angular/core';
+import { FormControl, NgForm, Validators } from '@angular/forms';
 import {MatDialog} from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog/dialog-ref';
+import { UserService } from '../../services/user.service';
 import { SignUpComponent } from '../signup/signup.component';
 
 @Component({
@@ -9,20 +11,50 @@ import { SignUpComponent } from '../signup/signup.component';
   styleUrls: [ './signin.component.css' ]
 })
 export class SignInComponent  {
-  email = new FormControl('', [Validators.required, Validators.email]);
+  username = new FormControl('');
+  password = new FormControl('');
 
-  constructor(public dialog: MatDialog) {}
+  messageError;
+  messageLoading = "MASUK";
 
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
+  isLoading = false
+  @ViewChild('signinForm') form: NgForm
+  constructor(public dialog: MatDialog, private userService: UserService, public dialogRef: MatDialogRef<SignInComponent>) {}
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
   submit(){
     window.location.reload()
   }
+  
+  onSubmitSignIn() {
+    this.isLoading = true
+    this.messageLoading = "Memeriksa Data ..."
+    this.userService.login(this.username.value, this.password.value).subscribe(
+      data => {
+        if(data.object.type == 'ADMIN'){
+          this.isLoading = false
+          localStorage.setItem("jwt", data.object.token)
+          localStorage.setItem("role", data.object.roles)
+          this.loadProfil(data.object)
+          this.messageLoading = "MASUK"
+        }else{
+          this.isLoading = false
+          this.messageError = "Maaf, Anda tidak memiliki akses ..."
+          this.messageLoading = "MASUK"
+        }
+      },
+      error => {
+        this.isLoading = false
+        this.messageError = error
+        this.messageLoading = "MASUK"
+      }
+    )
+  }
+
+  loadProfil(value) {
+    this.userService.setRefresh(value);
+    this.dialogRef.close(true)
+  }
+
   createAccount() {
     const dialogRef = this.dialog.open(SignUpComponent,{
       width: "600px",
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,51 +2,63 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { map, catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { AppConstant } from '../constant/AppConstant';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-  constructor(private http: HttpClient, private router: Router) {
-  }
+  private refresh: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  constructor(private http: HttpClient, private router: Router) {}
   login(username: string, password: string) {
-    const headers = new HttpHeaders().set("Content-Type", "application/json").set("Token", localStorage.getItem("jwt"));
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Token', localStorage.getItem('jwt'));
     let url = AppConstant.PROJECT_SERVICE_ENDPOINT + AppConstant.API_LOGIN;
-    return this.http.post(url,
-      {
-        "username": username,
-        "password": password,
-      }
-    ).pipe(
-      map((response) => {
-        let res = JSON.parse(JSON.stringify(response));
-        if (res.returnValue == '200') {
-          return res;
-        } else if (res.returnValue == '002') {
-          localStorage.removeItem("jwt")
-          this.router.navigate(["/auth/signin/"])
-          throw new Error(res.message);
-        }
-        else {
-          throw new Error(res.message);
-        }
-      }),
-      catchError((e: Response) => this.handleError(e))
-    );
+    return this.http
+      .post(url, {
+        username: username,
+        password: password,
+      })
+      .pipe(
+        map((response) => {
+          let res = JSON.parse(JSON.stringify(response));
+          if (res.returnValue == '200') {
+            return res;
+          } else if (res.returnValue == '002') {
+            localStorage.removeItem('jwt');
+            this.router.navigate(['/auth/signin/']);
+            throw new Error(res.message);
+          } else {
+            throw new Error(res.message);
+          }
+        }),
+        catchError((e: Response) => this.handleError(e))
+      );
+  }
+
+  public getRefresh(): Observable<any> {
+    return this.refresh.asObservable();
+  }
+
+  public setRefresh(value: any): void {
+    localStorage.setItem('currentUser', JSON.stringify(value));
+    this.refresh.next(value);
   }
 
   private handleError(error: any) {
-    let errMsg = (error.message) ? error.message :
-      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    let errMsg = error.message
+      ? error.message
+      : error.status
+      ? `${error.status} - ${error.statusText}`
+      : 'Server error';
     console.error(errMsg); // log to console instead
     if (error.status == 401 || error.status == 403) {
-      localStorage.removeItem("jwt")
-      this.router.navigate(["/auth/signin"])
+      localStorage.removeItem('jwt');
+      this.router.navigate(['/auth/signin']);
     } else {
       return throwError(errMsg);
     }
   }
-
 }
