60cd28135e82eeb6dc230f16136729d9a4c10d73
arm02 <arm.adrian02@gmail.com>
2022-03-01 00:47:31 +0700

    4 files modified

--- src/app/content/detail/detail.component.ts
+++ src/app/content/detail/detail.component.ts
@@ -1,8 +1,10 @@
-import { Component, EventEmitter, HostListener, Input, Output, VERSION } from '@angular/core';
+import { Component, IterableDiffers } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 import { NetworkService } from '../../services/network.service';
 import { NetworkData } from '../../models/network-data';
+import { UserService } from '../../services/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'detail-component',
@@ -10,19 +12,51 @@ import { NetworkData } from '../../models/network-data';
   styleUrls: ['./detail.component.css'],
 })
 export class DetailComponent {
-
+  currentUser = null;
+  query: string;
+  differ: any;
   networkData = new NetworkData
+  idNetwork: number = null
+  private subscriptionName: Subscription;
   constructor(private route: ActivatedRoute,
+    private userService: UserService,
+    differs: IterableDiffers,
     private networkService: NetworkService){
     const idNetwork: number = route.snapshot.params.id
+    this.idNetwork = idNetwork
+    this.differ = differs.find([]).create(null);
+      this.userService.getRefresh().subscribe((value: any) => {
+        if (value) {
+          this.currentUser = value;
+          if(this.currentUser){
+          }
+        }
+      });
     this.loadNetwork(idNetwork)
+  }
+
+  ngDoCheck() {
+    const change = this.differ.diff([this.currentUser]);
+    if (change) {
+      this.userService.getRefresh().subscribe((value: any) => {
+        if (value) {
+          this.currentUser = value;
+        }
+      });
+    }
+  }
+
+  loadFromUpload(){
+    this.subscriptionName= this.networkService.getUpdate().subscribe
+    (message => { 
+      this.loadNetwork(this.idNetwork)
+    });
   }
 
   loadNetwork(id: number){
     this.networkService.getNetwork(id).subscribe(
       data => {
         this.networkData = data.object
-        console.log(this.networkData)
       }, error => {
         console.log(error)
       }
